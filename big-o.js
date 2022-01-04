420ff5671305f6905fde89dbb76255e04334a850
Xinyi <xinyiyao84@yahoo.com>
2022-01-04 00:20:47 -0600

    interview practice

--- big-o.js
+++ big-o.js
@@ -1,1 +1,11 @@
+// const nemo = ['nemo']
+
+// function findNemo(nemo) {
+//     for (let i = 0; i < nemo.length; i++) {
+//         if (nemo[i] === 'nemo') {
+//             console.log('FOUND NEMO')
+//         }
+//     }
+// }
+
 
